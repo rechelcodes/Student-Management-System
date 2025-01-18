dc238ed93c7707d9e9441399ff41b5004372b1f4
Rechel Chandrika <rechel.chandrika04@gmail.com>
2025-01-18 13:52:04 +0600

    Update Student Management System.py

--- Student Management System.py
+++ Student Management System.py
@@ -1,91 +1,94 @@
-FILE_NAME = "students.txt"
+class StudentManagementSystem:
+    FILE_NAME = "students.txt"
+    students = []
 
-students = []
+    def load_students(self):
+        try:
+            with open(StudentManagementSystem.FILE_NAME, "r") as file:
+                for line in file:
+                    line = line.strip()
+                    if line:
+                        name, id, department, degree = line.split(",")
+                        StudentManagementSystem.students.append({"name": name, "id": id, "department": department, "degree": degree})
+                print("\nStudent data loaded successfully!")
+        except FileNotFoundError:
+            print("\nNo existing student data found. Starting fresh.")
 
-def load_students():
-    try:
-        with open(FILE_NAME, "r") as file:
-            for line in file:
-                line = line.strip()
-                if line:
-                    name, id, department, degree = line.split(",")
-                    students.append({"name": name, "id": id, "department": department, "degree": degree})
-            print("\nStudent data loaded successfully!")
-    except FileNotFoundError:
-        print("\nNo existing student data found. Starting fresh.")
+    def save_students(self):
+        with open(StudentManagementSystem.FILE_NAME, "w") as file:
+            for student in StudentManagementSystem.students:
+                file.write(f"{student['name']},{student['id']},{student['department']},{student['degree']}\n")
+        print("\nStudent data saved successfully!")
 
-def save_students():
-    with open(FILE_NAME, "w") as file:
-        for student in students:
-            file.write(f"{student['name']},{student['id']},{student['department']},{student['degree']}\n")
-    print("\nStudent data saved successfully!")
+    def add_student(self):
+        name = input("Enter student name: ")
+        id = input("Enter student ID: ")
+        department = input("Enter student department: ")
+        degree = input("Enter student degree: ")
+        student = {"name": name, "id": id, "department": department, "degree": degree}
+        StudentManagementSystem.students.append(student)
+        self.save_students()
+        print("\nStudent added successfully!")
 
-def add_student():
-    name = input("Enter student name: ")
-    id = input("Enter student ID: ")
-    department = input("Enter student department: ")
-    degree = input("Enter student degree: ")
-    student = {"name": name, "id": id, "department": department, "degree": degree}
-    students.append(student)
-    save_students()
-    print("\nStudent added successfully!")
+    def view_all_students(self):
+        if not StudentManagementSystem.students:
+            print("\nNo students to display.")
+        else:
+            print("\n" + "-" * 30)
+            print(f"{'Student List':^30}")
+            print("=" * 30)
+            for index, student in enumerate(StudentManagementSystem.students, start=1):
+                print(f"\nStudent {index}")
+                print("-" * 30)
+                print(f"Name: {student['name']}")
+                print(f"ID: {student['id']}")
+                print(f"Department: {student['department']}")
+                print(f"Degree: {student['degree']}")
+                print("-" * 30)
 
-def view_all_student():
-    if not students:
-        print("\nNo students to display.")
-    else:
-        print("\n" + "-" * 30)
-        print(f"{'Student List':^30}")
-        print("=" * 30)
-        for index, student in enumerate(students, start=1):
-            print(f"\nStudent {index}")
+    def search_student(self):
+        roll = input("\nEnter the ID you want to search: ")
+        found = [student for student in StudentManagementSystem.students if student["id"] == roll]
+        if found:
+            print("\nStudent found!")
             print("-" * 30)
-            print(f"Name: {student['name']}")
-            print(f"ID: {student['id']}")
-            print(f"Department: {student['department']}")
-            print(f"Degree: {student['degree']}")
-            print("-" * 30)
+            for student in found:
+                print(f"Name: {student['name']}")
+                print(f"ID: {student['id']}")
+                print(f"Department: {student['department']}")
+                print(f"Degree: {student['degree']}")
+                print("-" * 30)
+        else:
+            print("\nStudent not found.")
 
-def search_student():
-    roll = input("\nEnter the ID you want to search: ")
-    found = [student for student in students if student["id"] == roll]
-    if found:
-        print("\nStudent found!")
-        print("-" * 30)
-        for student in found:
-            print(f"Name: {student['name']}")
-            print(f"ID: {student['id']}")
-            print(f"Department: {student['department']}")
-            print(f"Degree: {student['degree']}")
-            print("-" * 30)
-    else:
+    def edit_student(self):
+        id = input("Enter the student ID you want to edit: ")
+        for student in StudentManagementSystem.students:
+            if student["id"] == id:
+                new_name = input("Enter New Name: ")
+                new_department = input("Enter New Department: ")
+                new_degree = input("Enter New Degree: ")
+                new_id = input("Enter New ID: ")
+                student.update({"name": new_name, "id": new_id, "department": new_department, "degree": new_degree})
+                self.save_students()
+                print("\nStudent profile updated successfully!")
+                return
         print("\nStudent not found.")
 
-def edit_student():
-    id = input("Enter the student ID you want to edit: ")
-    for student in students:
-        if student["id"] == id:
-            new_name = input("Enter New Name: ")
-            new_department = input("Enter New Department: ")
-            new_degree = input("Enter New Degree: ")
-            new_id = input("Enter New ID: ")
-            student.update({"name": new_name, "id": new_id, "department": new_department, "degree": new_degree})
-            save_students()
-            print("\nStudent profile updated successfully!")
-            return
-    print("\nStudent not found.")
+    def delete_student(self):
+        id = input("Enter the student ID you want to delete: ")
+        for student in StudentManagementSystem.students:
+            if student["id"] == id:
+                StudentManagementSystem.students.remove(student)
+                self.save_students()
+                print("\nStudent has been removed.")
+                return
+        print("\nStudent not found.")
 
-def delete_student():
-    id = input("Enter the student ID you want to delete: ")
-    for student in students:
-        if student["id"] == id:
-            students.remove(student)
-            save_students()
-            print("\nStudent has been removed.")
-            return
-    print("\nStudent not found.")
 
-load_students()
+system = StudentManagementSystem()
+system.load_students()
+
 while True:
     print("\n" + "=" * 40)
     print(f"{'Student Management System':^40}")
@@ -101,18 +104,17 @@ while True:
     print("=" * 40)
     choice = input("Enter your choice: ")
     if choice == "1":
-        add_student()
+        system.add_student()
     elif choice == "2":
-        view_all_student()
+        system.view_all_students()
     elif choice == "3":
-        search_student()
+        system.search_student()
     elif choice == "4":
-        edit_student()
+        system.edit_student()
     elif choice == "5":
-        delete_student()
+        system.delete_student()
     elif choice == "6":
         print("\nExiting the program. Goodbye!")
         break
     else:
         print("\nInvalid choice. Try again.")
-        
